afd57b870fd15e962cd8b1f32f26a3288d61b722
진명 박 <pjm9252@naver.com>
2018-09-18 13:12:37 +0900

    d

--- GameFramework/PP03.GameClass/Game.cpp
+++ GameFramework/PP03.GameClass/Game.cpp
@@ -6,29 +6,49 @@ bool Game::init(const char* title, int xpos, int ypos, int width, int height, bo
 
 	if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
 	{
+
+		
+
 		m_pWindow = SDL_CreateWindow(title, xpos, ypos, width, height, fullscreen);
 		if (m_pWindow != 0)
 		{
 			m_pRenderer = SDL_CreateRenderer(m_pWindow, -1, 0);
 		}
+
+		SDL_Surface* pTempSurface = SDL_LoadBMP("assets/rider.bmp");
+
+		m_pTexture = SDL_CreateTextureFromSurface(m_pRenderer, pTempSurface);
+
+		SDL_FreeSurface(pTempSurface);
+
+		SDL_QueryTexture(m_pTexture, NULL, NULL, &m_sourceRectangle.w, &m_sourceRectangle.h);
+
+
+
 		m_bRunning = true;
 	}
 	else
 	{
 		return false;
 	}
+
+
+
 	return true;
 }
 
+
 void Game::render()
 {
-	//m_destinationRectangle.x = m_sourceRectangle.x = 0;
-	//m_destinationRectangle.y = m_sourceRectangle.y = 0;
-	//m_destinationRectangle.w = m_sourceRectangle.w;
-	//m_destinationRectangle.h = m_sourceRectangle.h = 0;
+
+
+	m_destinationRectangle.x = m_sourceRectangle.x = 0;
+	m_destinationRectangle.y = m_sourceRectangle.y = 0;
+	m_destinationRectangle.w = m_sourceRectangle.w;
+	m_destinationRectangle.h = m_sourceRectangle.h;
 
 	SDL_RenderClear(m_pRenderer);
-	//SDL_RenderCopy(m_pRenderer, m_pTexture, &m_sourceRectangle, &m_destinationRectangle);
+	SDL_RenderCopy(m_pRenderer, m_pTexture, &m_sourceRectangle, &m_destinationRectangle);
 
 	SDL_RenderPresent(m_pRenderer);
 
@@ -63,11 +83,15 @@ void Game::handleEvents()
 		
 	}
 }
+
 /*
-		
-SDL_Surface* pTempSurface = SDL_LoadBMP("assets/rider.bmp");
+SDL_Surface* pTempSurface = SDL_LoadBMP("Assets/rider.bmp");
 m_pTexture = SDL_CreateTextureFromSurface(m_pRenderer, pTempSurface);
 
 SDL_FreeSurface(pTempSurface);
 
 SDL_QueryTexture(m_pTexture, NULL, NULL, &m_sourceRectangle.w, &m_sourceRectangle.h);*/
+
+
+
+/**/
--- GameFramework/PP03.GameClass/Game.h
+++ GameFramework/PP03.GameClass/Game.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "SDL.h"
 
+
+
 class Game
 {
 public:
@@ -19,8 +21,9 @@ private:
 	SDL_Window* m_pWindow;
 	SDL_Renderer* m_pRenderer;
 	bool m_bRunning;
+	
+
 	SDL_Texture* m_pTexture;
 	SDL_Rect m_sourceRectangle;
 	SDL_Rect m_destinationRectangle;
-
 };
