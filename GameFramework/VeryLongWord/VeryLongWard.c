ef06e950e247cebcab59cd508e7b1d8ec470a0d6
user <user@532-22>
2018-09-20 18:09:37 +0900

    dddd

---
+++ GameFramework/VeryLongWord/VeryLongWard.c
@@ -0,0 +1,27 @@
+#include <stdio.h> 
+
+void VeryLongWord(char * sen) {
+
+	char temp[]= sen;
+
+	int i = 0;
+
+	while (temp[i] == NULL)
+	{
+		for (i = 0;temp[i] == " " ;i++)
+		{
+			char temp2 = 
+		}
+	}
+
+	printf("%s", sen);
+
+}
+
+int main(void) {
+
+	// keep this function call here
+	VeryLongWord("i love dogs"); // 큰 따옴표 잘 넣어보기 
+	return 0;
+
+}
