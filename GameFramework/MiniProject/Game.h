8ce68ad01f27d9ee838bf0a2b6e2cff040a4f649
진명 박 <pjm9252@naver.com>
2018-10-06 14:36:59 +0900

    f

--- GameFramework/MiniProject/Game.h
+++ GameFramework/MiniProject/Game.h
@@ -26,19 +26,23 @@ private:
 	SDL_Renderer * m_pRenderer;
 	SDL_Window * m_pWindow;
 	
-	SDL_Surface* pTempSurface;
-	SDL_Surface* pTempSurface2;
-	
 	bool m_bRunning;
 
+	
+
+};
+
+
+/*SDL_Surface* pTempSurface;
+	SDL_Surface* pTempSurface2;
+	
 	SDL_Texture* m_pTexture;
 	SDL_Texture* m_pTexture2;
+
 	SDL_Rect m_background;
 	SDL_Rect m_destinback;
 	
 
 	SDL_Rect m_source;
 	SDL_Rect m_destin;
-
-
-};
+	*/
