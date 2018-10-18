aeccf69c9c8a7e1b370f2f8de35a8064b3861f9c
user <user@532-28>
2018-10-18 17:56:01 +0900

    pp10 player

--- GameFramework/PP10.Polymorphism/Player.h
+++ GameFramework/PP10.Polymorphism/Player.h
@@ -5,9 +5,6 @@
 class Player : public GameObject
 {
 public:
-	void load(int x, int y, int width, int height,
-		std::string textureID);
-	void draw(SDL_Renderer* pRenderer);
 	void update();
-	//void clean();
+
 };
