a0134287bdf51d8e0268d430182f36aa67bd6cde
user <user@532-28>
2018-10-18 17:53:10 +0900

    pp10

---
+++ GameFramework/PP10.Polymorphism/Game.h
@@ -0,0 +1,43 @@
+#pragma once
+#include "SDL.h"
+#include "TextureManager.h"
+#include "GameObject.h"
+#include "Player.h"
+#include "Enemy.h"
+#include <vector>
+
+class Game
+{
+public:
+	Game() {}
+	~Game() {}
+	bool init(const char* title, int xpos, int ypos,
+		int width, int height, bool fullscreen);
+	void render();
+	void update();
+	void handleEvents();
+	void clean();
+	bool running() { return m_bRunning; }
+
+	int m_currentFrame;
+	int m2_currentFrame;
+	//TextureManager m_textureManager;
+	std::vector<GameObject*> m_gameObjects;
+
+
+	GameObject* m_go;
+	GameObject* m_player;
+	GameObject* m_enemy;
+
+private:
+
+	
+
+	SDL_Window* m_pWindow;
+	SDL_Renderer* m_pRenderer;
+	bool m_bRunning;
+
+	//SDL_Texture* m_pTexture;
+	//SDL_Rect m_sourceRectangle;
+	//SDL_Rect m_destinationRectangle;
+};
