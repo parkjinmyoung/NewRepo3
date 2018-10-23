9e5af80a251a5b242b98f26bfb18a8967c0dad2a
진명 박 <pjm9252@naver.com>
2018-10-23 15:34:02 +0900

    moono.png

--- GameFramework/PP10.Polymorphism/Enemy.h
+++ GameFramework/PP10.Polymorphism/Enemy.h
@@ -7,13 +7,7 @@ class Enemy : public GameObject
 private:
 	int speed = 1;
 	int temp = 0;
-
 public:
 	Enemy(int s);
-	//void load(int x, int y, int width, int height,
-	//	std::string textureID);
-	//void draw(SDL_Renderer* pRenderer);
 	void update();
-	//void clean();
-
 };
