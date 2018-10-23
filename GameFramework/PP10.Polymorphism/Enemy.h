2e7f5b1760aca01583f82a23d9dfc91388f738c7
user <user@532-22>
2018-10-23 13:20:31 +0900

    hh05

--- GameFramework/PP10.Polymorphism/Enemy.h
+++ GameFramework/PP10.Polymorphism/Enemy.h
@@ -3,10 +3,17 @@
 
 class Enemy : public GameObject
 {
+
+private:
+	int speed = 1;
+	int temp = 0;
+
 public:
+	Enemy(int s);
 	//void load(int x, int y, int width, int height,
 	//	std::string textureID);
 	//void draw(SDL_Renderer* pRenderer);
 	void update();
 	//void clean();
+
 };
--- GameFramework/PP10.Polymorphism/Game.h
+++ GameFramework/PP10.Polymorphism/Game.h
@@ -28,6 +28,7 @@ public:
 	GameObject* m_go;
 	GameObject* m_player;
 	GameObject* m_enemy;
+	GameObject* m_enemy2;
 
 private:
 
