1f9066890c2aa6624f4083adc5d77ca5bd94417c
진명 박 <pjm9252@naver.com>
2018-10-25 18:50:42 +0900

    Revert "pp12"
    This reverts commit fa07b347cee6b5847f5e274b3356ca9e2b0ba130.

--- GameFramework/PP12.Vector2D/Vector2D.h
+++ GameFramework/PP12.Vector2D/Vector2D.h
@@ -9,7 +9,10 @@ public:
 	float getY() { return m_y; }
 	void setX(float x) { m_x = x; }
 	void setY(float y) { m_y = y; }
+private:
 
+	float m_x;
+	float m_y;
 
 	Vector2D operator+(const Vector2D& v2) const
 	{
@@ -70,12 +73,5 @@ public:
 			(*this) *= 1 / l;
 		}
 	}
-
-private:
-
-	float m_x;
-	float m_y;
-
-	
 };
 
