4ad171e0f8f297a92c81d2f92af1a71e32789400
user <user@532-28>
2018-10-25 18:02:07 +0900

    pp12

--- GameFramework/PP12.Vector2D/SDLGameObject.h
+++ GameFramework/PP12.Vector2D/SDLGameObject.h
@@ -1,6 +1,10 @@
 #pragma once
 #include "GameObject.h"
 
+#include "Vector2D.h"
+
+
+
 class SDLGameObject : public GameObject
 {
 public:
@@ -9,8 +13,10 @@ public:
 	virtual void update();
 	virtual void clean();
 protected:
-	int m_x;
-	int m_y;
+	Vector2D m_position;
+	Vector2D m_velocity;
+	Vector2D m_acceleration;
+
 	int m_width;
 	int m_height;
 	int m_currentRow;
--- GameFramework/PP12.Vector2D/main.cpp
+++ GameFramework/PP12.Vector2D/main.cpp
@@ -1,7 +1,9 @@
 #include "Game.h"
 
 //Game* g_gmae = 0;
-
+const int FPS = 60;
+const int DELAY_TIME = 1000.0f / FPS;
+Uint32 frameStart, frameTime;
 
 int main(int argc, char* argv[])
 {
@@ -11,11 +13,18 @@ int main(int argc, char* argv[])
 		std::cout << "game init success!\n";
 		while (TheGame::Instance()->running())
 		{
+			frameStart = SDL_GetTicks();
 			TheGame::Instance()->handleEvents();
 			TheGame::Instance()->update();
 			TheGame::Instance()->render();
-			SDL_Delay(10);
+			frameTime = SDL_GetTicks() - frameStart;
+
+			if (frameTime < DELAY_TIME)
+			{
+				SDL_Delay((int)(DELAY_TIME - frameTime));
+			}
 		}
+
 	}
 	//thegame인스턴스실패
 	else {
