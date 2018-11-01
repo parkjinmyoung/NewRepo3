63000a1ba8c5d16ef47d32a0d05848c2f9057d88
user <user@532-28>
2018-11-01 18:07:20 +0900

    pp14

---
+++ GameFramework/PP14.MInputHandler/GameObject.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "SDL.h"
+#include "SDL_image.h"
+#include <iostream>
+#include "TextureManager.h"
+#include "LoaderParams.h"
+
+
+class GameObject
+{
+public:
+	virtual void draw() = 0;
+	virtual void update() = 0;
+	virtual void clean() = 0;
+protected:
+	GameObject(const LoaderParams* pParams) {}
+	virtual ~GameObject() {}
+};
---
+++ GameFramework/PP14.MInputHandler/main.cpp
@@ -0,0 +1,40 @@
+#include "Game.h"
+
+//Game* g_gmae = 0;
+const float FPS = 60.0f;
+const float DELAY_TIME = 1000.0f / FPS;
+Uint32 frameStart, frameTime;
+
+
+int main(int argc, char* argv[])
+{
+	std::cout << "game init attempt...\n";
+	if (TheGame::Instance()->init("Chapter 14", 100, 100, 640, 480, false))
+	{
+		std::cout << "game init success!\n";
+		while (TheGame::Instance()->running())
+		{
+			frameStart = SDL_GetTicks();
+			TheGame::Instance()->handleEvents();
+			TheGame::Instance()->update();
+			TheGame::Instance()->render();
+			frameTime = SDL_GetTicks() - frameStart;
+
+			if (frameTime < DELAY_TIME)
+			{
+				SDL_Delay((int)(DELAY_TIME - frameTime));
+			}
+		}
+
+	}
+	//thegame인스턴스실패
+	else {
+		std::cout << "game init failure - " << SDL_GetError() << "\n";
+		return -1;
+	}
+	
+	std::cout << "game closing...\n";
+	TheGame::Instance()->clean();
+	return 0;
+}
+
