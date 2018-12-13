f3b1984e5e2f7668b2823017862a47cf6131b1e9
진명 박 <pjm9252@naver.com>
2018-12-13 22:31:08 +0900

    12

--- GameFramework/PP19/GameState.h
+++ GameFramework/PP19/GameState.h
@@ -8,7 +8,8 @@ class GameState
 
 public:
 	std::vector<GameObject*> m_gameObjects;
-
+	std::vector<SDLGameObject*> m_WallObjects;
+	int GameObjectsOrder = 0;
 	virtual void update() = 0;
 	virtual void render() = 0;
 	virtual bool onEnter() = 0; 
--- GameFramework/PP19/StateDecorate.h
+++ GameFramework/PP19/StateDecorate.h
@@ -1,13 +1,40 @@
 #pragma once
 #include "SDLGameState.h"
+#include "Game.h"
+#include "GameOverState.h"
 
 class StateDecorate : public SDLGameState
 {
 public: 
-	StateDecorate();
+	StateDecorate(SDLGameState* decstate)
+	{
+		decoratedstate = decstate;
+
+	}
 
 
 private:
 
+	SDLGameState * decoratedstate;
 
+	virtual void update()
+	{
+		decoratedstate->update();
+	}
+	virtual void render()
+	{
+		decoratedstate->render();
+	}
+	virtual bool onEnter()
+	{
+		return decoratedstate->onEnter();
+	}
+
+	virtual bool onExit()
+	{
+		return decoratedstate->onExit();
+	}
+
+
+	
 };
